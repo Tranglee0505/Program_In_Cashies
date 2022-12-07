f5f25df1cb8d782d8572433cf7fb8599267996cb
TrangLee <trangle0505.tt@gmail.com>
2022-12-07 23:36:21 +0700

    Video_Program

--- class.py
+++ class.py
@@ -1,36 +1,62 @@
-# class Video:
-#     def __init__(self,title,link):
-#         self.title = title
-#         self.link = link
+class Video:
+	def __init__(self, title, link):
+		self.title = title
+		self.link = link
 
-# def read_video():
-#     title = input("Enter title :")
-#     link = input("Enter link :")
-#     video = Video(title,link)
+def read_video():
+	title = input("Enter title: ")
+	link = input("Enter link: ")
+	video = Video(title, link)
+	return video
 
-# def read_videos():
-#     videos = []
-#     total_video = int(input("Enter how many video :"))
-#     for i in range(total_video):
-#         print(" Enter video", i+1)
-#         vid = read_video()
-#         videos.append(vid)
+def print_video(video):
+	print("Video title: ", video.title, end="")
+	print("Video link: ", video.link, end="")
 
-# def print_video(video):
-#     print("Video title :", video.title)
-#     print("Video link :", video.link)
+def read_videos():
+	videos = []
+	total_video = int(input("Enter how many videos: "))
+	for i in range(total_video):
+		print("Enter video ", i+1)
+		vid = read_video()
+		videos.append(vid)
+	return videos
 
-# def main():
-#     total_video = int(input("Enter how many videos:"))
-#     vid = read_video()
-#     print_video(vid)
+def print_videos(videos):
+	print("---")
+	for i in range(len(videos)):
+		print_video(videos[i])
 
-# main()
+def write_video_txt(video, file):
+	file.write(video.title + "\n")
+	file.write(video.link + "\n")
 
+def write_videos_txt(videos):
+	total = len(videos)
+	with open("data.txt", "w") as file:
+		file.write(str(total) + "\n")
+		for i in range(total):
+			write_video_txt(videos[i], file)
 
-p = float(input('Số tiền gốc ban đầu: '))
-t = float(input('Số năm cho vay: '))
-r = float(input('Lãi suất danh nghĩa hàng năm: '))
-n = float(input('Số lần ghép lãi trong một năm: '))
-a = p*(1+(r/n))**(n*t)
-print('Tổng số tiền là: ', a)
+def read_video_from_txt(file):
+	title = file.readline()
+	link = file.readline()
+	video = Video(title, link)
+	return video
+
+def read_videos_from_txt():
+	videos = []
+	with open("data.txt", "r") as file:
+		total = file.readline()		
+		for i in range(int(total)):
+			video = read_video_from_txt(file)
+			videos.append(video)
+	return videos
+
+def main():
+	videos = read_videos()
+	write_videos_txt(videos)	
+	videos = read_videos_from_txt()
+	print_videos(videos)
+
+main()
